db45935f5b20b4f5e2c76e5f9cecf50be1fcfdf8
iamjoydevdas <iamjoydevdas@gmail.com>
2025-05-12 22:32:12 +0100

    fix build

--- src/main/java/com/toteall/ui/discovery/UIDiscoveryApplication.java
+++ src/main/java/com/toteall/ui/discovery/UIDiscoveryApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(Properties.class)
 @SpringBootApplication
-public class LocationsServiceApplication {
+public class UIDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LocationsServiceApplication.class, args);
+		SpringApplication.run(UIDiscoveryApplication.class, args);
 	}
 }
